aa1e5b1770af305849e874561566a83518b814b7
Joshua Li <joshuarli98@gmail.com>
2022-01-28 11:46:54 -0800

    wip

--- src/sentry/runner/commands/migrations.py
+++ src/sentry/runner/commands/migrations.py
@@ -11,9 +11,10 @@ def migrations():
 @migrations.command()
 @click.argument("app_name")
 @click.argument("migration_name")
+@click.option("--big-red-button", is_flag=True)
 @configuration
 @click.pass_context
-def run(ctx, app_name, migration_name):
+def run(ctx, app_name, migration_name, big_red_button):
     "Manually run a single data migration. Will error if migration is not data only."
 
     from django.apps import apps
@@ -24,11 +25,17 @@ def run(ctx, app_name, migration_name):
     migration = MigrationExecutor(connections["default"]).loader.get_migration_by_prefix(
         app_name, migration_name
     )
-    for op in migration.operations:
-        if not isinstance(op, RunPython):
-            raise click.ClickException(
-                "Migration must contain only RunPython ops, found: %s" % type(op).__name__
-            )
 
+    if not big_red_button:
+        for op in migration.operations:
+            if not isinstance(op, RunPython):
+                raise click.ClickException(
+                    f"""
+Migration must contain only RunPython ops, found: {type(op).__name__}
+If you know what you're doing, you can bypass this with sentry migrations run --big-red-button.
+"""
+                )
+
+    # TODO: some ops like AlterIndexTogether just don't have code attr.
     for op in migration.operations:
         op.code(apps, None)
